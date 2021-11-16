a3e9fc4eab4c30329bcf9585859dcaae05e25517
Alan Kewem <kewemcg@gmail.com>
2021-11-16 00:17:19 -0400

    added parameters in new classes to the refactored list of transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,13 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() =>
   runApp(MaterialApp(
     home: Scaffold(
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              title: Text("R\$ 100,00"),
-              subtitle: Text('Ifood: Taki Kando'),
-              leading: Icon(Icons.money_off),
-
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text("R\$ 458,00"),
-              subtitle: Text('Venda: Conjunto Del Rey'),
-              leading: Icon(Icons.monetization_on),
-
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text("R\$ 356,00"),
-              subtitle: Text('Contas fixas: Água'),
-              leading: Icon(Icons.money_off),
-
-            ),
-          ),
-        ],
-      ),
+      body: ListaTransferencias(),
       appBar: AppBar(
         title: Text('Transferências'),),
         floatingActionButton: FloatingActionButton(
@@ -41,3 +16,44 @@ void main() =>
         ),
     )
   ));
+
+  class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          ItemTransferencia(Transferencia(283.45, 23333)),
+          ItemTransferencia(Transferencia(344.00, 19093)),
+          ItemTransferencia(Transferencia(3444.11, 24093)),
+        ],
+      );
+  }
+  }
+
+  class ItemTransferencia extends StatelessWidget {
+
+    final Transferencia _transferencia;
+
+    const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+            child: ListTile(
+              title: Text("R\$ ${_transferencia.valor.toString()}"),
+              subtitle: Text('Conta ID: ${_transferencia.numeroConta.toString()}'),
+              leading: Icon(Icons.money_off,
+                color: Colors.red,),
+            ),
+          );
+  }
+  }
+
+  class Transferencia {
+    final double valor;
+    final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+  }
